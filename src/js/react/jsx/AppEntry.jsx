a398a4e838a1c30529a380710ed58cd8c2db87a6
June YAMAMOTO <yamamoto@questetra.com>
2017-12-26 18:50:53 +0900

    test

--- src/js/react/jsx/AppEntry.jsx
+++ src/js/react/jsx/AppEntry.jsx
@@ -19,18 +19,6 @@ ReactDOM.render(
 			<LayoutGridCell><Card img="https://placehold.jp/150x150.png" /></LayoutGridCell>
 			<LayoutGridCell><Card /></LayoutGridCell>
 			<LayoutGridCell>
-				<div className="mdc-form-field">
-				  <div className="mdc-checkbox">
-				    <input type="checkbox" id="my-checkbox" className="mdc-checkbox__native-control"/>
-				    <div className="mdc-checkbox__background">
-				      <svg className="mdc-checkbox__checkmark" viewBox="0 0 24 24">
-				        <path className="mdc-checkbox__checkmark__path" fill="none" stroke="white" d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
-				      </svg>
-				      <div className="mdc-checkbox__mixedmark"></div>
-				    </div>
-				  </div>
-				  <label htmlFor="my-checkbox" id="my-checkbox-label">This is my checkbox</label>
-				</div>
 				<TextField id="context-input" type="text" label="Context URL" placeholder="https://" required pattern=".{8,}"/>
 			</LayoutGridCell>
 
