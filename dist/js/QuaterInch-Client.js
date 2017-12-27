745b4961d65fb0a99121014bcdaac649abc173b9
June YAMAMOTO <yamamoto@questetra.com>
2017-12-27 16:18:33 +0900

    Oauth2

--- dist/js/QuaterInch-Client.js
+++ dist/js/QuaterInch-Client.js
@@ -19684,35 +19684,58 @@ var Dispatcher = require('flux').Dispatcher;
 var dispatcher = new Dispatcher();
 
 var Action = {
-	authentication:function(clientId, clientSecret, authorizeEndpoint, tokenEndpoint, scope){
-		dispatcher.dispatch({
-			actionType: "authentication",
-			value:{
-				clientId : clientId,
-				clientSecret : clientSecret,
-				authorizeEndpoint : authorizeEndpoint,
-				tokenEndpoint : tokenEndpoint,
-				scope : scope
-			}
-		});
-	}
+    authentication: function(clientId, clientSecret, authorizeEndpoint, tokenEndpoint, scope) {
+        dispatcher.dispatch({
+            actionType: "authentication",
+            value: {
+                clientId: clientId,
+                clientSecret: clientSecret,
+                authorizeEndpoint: authorizeEndpoint,
+                tokenEndpoint: tokenEndpoint,
+                scope: scope
+            }
+        });
+    }
 }
 
+var Strage = function() {
+    function _save(name, value) {
+        localStorage.setItem(name, JSON.stringify(value));
+    }
+
+    return {
+        save: function(name, value) {
+            _save(name, value);
+        }
+    }
+}
+
+(function() {
+    var arg = new Object;
+    var pair = location.search.substring(1).split('&');
+    for (var i = 0; pair[i]; i++) {
+        var kv = pair[i].split('=');
+        arg[kv[0]] = kv[1];
+    }
+    console.log(arg);
+}());
+
 var Store = assign({}, EventEmitter.prototype, {
-	dispatcherIndex: dispatcher.register(function(payload) {
-		switch (payload.actionType) {
-			case "authentication":
-				console.log(payload.value);
-				var url = payload.value.authorizeEndpoint;
-				url += "?client_id=" + encodeURI(payload.value.clientId);
-				url += "&redirect_uri=" + encodeURI('https://quarter-inch.github.io/Client/dist/oauth2-redirect.html');
-				url += "&scope=" + encodeURI(payload.value.scope);
-				url += "&response_type=code";
-				console.log(url);
-				location.href = url;
-				break;
-		};
-	})
+    dispatcherIndex: dispatcher.register(function(payload) {
+        switch (payload.actionType) {
+            case "authentication":
+                Strage.save("authentication", payload.value);
+
+                var url = payload.value.authorizeEndpoint;
+                url += "?client_id=" + encodeURI(payload.value.clientId);
+                url += "&redirect_uri=" + encodeURI('https://quarter-inch.github.io/Client/dist/index.html');
+                url += "&scope=" + encodeURI(payload.value.scope);
+                url += "&response_type=code";
+
+                location.href = url;
+                break;
+        };
+    })
 });
 
 module.exports = {
